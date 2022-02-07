2d7806f16dd61918a6cedc0c03d0e12f6b55a123
karolina <karolina1998@vip.onet>
2022-02-07 20:54:26 +0100

    player script and prefab update

---
+++ Frogger/Assets/Scripts/Player.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+
+{
+    [SerializeField] private float speed;
+    private bool isMoving;
+    private Vector3 origPos;
+    private Vector3 targetPos;
+    private float timeToMove = 0.2f;
+
+
+    void Start()
+    {
+        
+    }
+
+  
+    void Update()
+    {
+        //Movement
+        if (Input.GetKey("up") && !isMoving)
+            StartCoroutine(MovePlayer(Vector3.up));
+
+        if (Input.GetKey("left") && !isMoving)
+            StartCoroutine(MovePlayer(Vector3.left));
+
+        if (Input.GetKey("down") && !isMoving)
+            StartCoroutine(MovePlayer(Vector3.down));
+
+        if (Input.GetKey("right") && !isMoving)
+            StartCoroutine(MovePlayer(Vector3.right));
+
+
+    }
+
+
+
+    private IEnumerator MovePlayer(Vector3 direction)
+    {
+        isMoving = true;
+        float elapsedTime = 0f;
+
+        origPos = transform.position;
+        targetPos = origPos + direction;
+
+        while(elapsedTime < timeToMove)
+        {
+            transform.position = Vector3.Lerp(origPos, targetPos, (elapsedTime / timeToMove));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = targetPos;
+
+        isMoving = false;
+    } 
+}
